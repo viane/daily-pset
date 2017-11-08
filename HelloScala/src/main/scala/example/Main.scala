71799af080e0e51ab3f34abf9d7febbf334bf91d
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-11-08 02:48:56 -0500

    test

--- HelloScala/src/main/scala/example/Main.scala
+++ HelloScala/src/main/scala/example/Main.scala
@@ -3,4 +3,10 @@ package example
 object Main extends App {
 val seq = Seq(1,2,5,2,3);
   println(s"The largest number in the sequent is ${seq.max}")
+
+  println(16.toHexString)
+
+  println((0 until 10).contains(10))
+
+  println("bar".take(2))
 }
