b50a0bc2699f73293e6e589e641c3d1fe9e58f39
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-03 23:50:13 -0500

    C#

---
+++ Generate-Parentheses/Generate-Parentheses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenerateParentheses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
