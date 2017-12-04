356af136b1905517b0656774e29c3d5fff29d268
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-03 21:23:13 -0500

    java

---
+++ Valid Parentheses/src/com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String test = "{([])[]}";
+        Solution s = new Solution();
+        System.out.println(s.isValid(test));
+    }
+}
