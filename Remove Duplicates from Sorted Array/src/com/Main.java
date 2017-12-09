f3a916ff907db5da28ac79d71543f5e6ddb1a422
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-12-09 07:12:59 -0500

    Java

---
+++ Remove Duplicates from Sorted Array/src/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] nums = {1,1,2,2,3,4};
+        Solution s = new Solution();
+        System.out.println(s.removeDuplicates(nums));
+    }
+}
