6c233ac5fa4d4792eeb8dbf9e21b8ef7e910e5be
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-02-02 02:10:51 -0500

    v1
    Functional for 2 more digits multiplication.
    
    Failed on single digit multiply, eg 5*5

--- Multiply Strings/src/com/Solver.java
+++ Multiply Strings/src/com/Solver.java
@@ -7,23 +7,62 @@ public class Solver {
 		checkInputs(s1,s2);
 		
 		int col_len = s1.length() + s2.length();
-		int row_len = s1.length() * s2.length();
+		int row_len = (s1.length() * s2.length()) < 2 ? 2 : s1.length() * s2.length();
+		
+		// 
 		
 		initMatrix(row_len, col_len);
 		
-		sum(s1,s2);
-		
-		displayMatrix();
-		return "";
+		return sum(s1,s2,row_len,col_len);
 	}
 	
-	private void sum(String s1, String s2) {
+	private String sum(String s1, String s2, int row_len, int col_len) {
 		int row_index= 0;
+		boolean carry_out = false;
+		
+		// place proper multiplyed digit into matrix
 		for(int s2_digit = s2.length()-1; s2_digit>=0;s2_digit--) {
 			for(int s1_digit = s1.length()-1; s1_digit>=0;s1_digit--,row_index++) {
+				int single_digit_multiply = (s1.charAt(s1_digit) - '0') * (s2.charAt(s2_digit) - '0');
+				if(carry_out) {
+					single_digit_multiply += 1;
+				}
+				carry_out = single_digit_multiply >= 10 ?  true : false;
+				single_digit_multiply = single_digit_multiply % 10;
 				
+				multiply_matrix[row_index][s2_digit+s1_digit+1] = (char) ('0'+ single_digit_multiply);
+						
+				System.out.printf("%d , %d => %s \n",row_index,s2_digit+s1_digit+1, single_digit_multiply);
+				
+				System.out.println(multiply_matrix[row_index][s2_digit+s1_digit+1]);
 			}
 		}
+		
+		StringBuilder sb = new StringBuilder();
+		carry_out = false;
+		for(int digit_col_index= col_len-1;digit_col_index>=0;digit_col_index--) {
+			int digit_sum_per_col = 0;
+			for(int digit_row_index = row_len-1;digit_row_index>=0;digit_row_index--) {
+				digit_sum_per_col += multiply_matrix[digit_row_index][digit_col_index] - '0';
+			}
+			digit_sum_per_col += carry_out ? 1 : 0;
+			
+			carry_out = digit_sum_per_col >= 10 ? true : false;
+			
+			digit_sum_per_col = digit_sum_per_col % 10;
+			
+			sb.insert(0, (char)('0'+digit_sum_per_col));
+			
+		}
+		
+		displayMatrix();
+		
+		if(sb.charAt(0) == '0') {
+			return sb.substring(1).toString();
+		}else {
+			return sb.toString();
+		}
+
 	}
 	
 	private void checkInputs(String s1, String s2) throws Throwable {
@@ -50,8 +89,7 @@ public class Solver {
 	private void displayMatrix() {
 		for(int row = 0; row<multiply_matrix.length;row++) {
 			for(int col = 0; col < multiply_matrix[row].length;col++) {
-				multiply_matrix[row][col]='0';
-				System.out.print(multiply_matrix[row][col]);
+				System.out.print(multiply_matrix[row][col] + " ");
 			}
 			System.out.print('\n');
 		}
--- Multiply Strings/src/com/SolverTest.java
+++ Multiply Strings/src/com/SolverTest.java
@@ -10,7 +10,12 @@ public class SolverTest {
 	@Test
 	public void testMultiply() {
 		Solver s = new Solver();
-		assertEquals("Should equal",s.multiply("123", "321"),"39483");
+		try {
+			assertEquals("Should equal","25",s.multiply("5", "5"));
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
