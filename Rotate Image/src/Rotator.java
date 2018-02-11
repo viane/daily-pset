e3d5694e32752b46c5a7d5b956cb8d113ee83862
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-02-11 14:31:30 -0500

    v1
    java

--- Rotate Image/src/Rotator.java
+++ Rotate Image/src/Rotator.java
@@ -1,4 +1,44 @@
 
 public class Rotator {
-
+    public int[][] rotate(int[][] matrix) {
+    		System.out.println("Input image:");
+    		displayImage(matrix);
+    		
+    		int[][] afterRotate = rotateMatrix(matrix);
+    		System.out.println("\nAfter Rotation:");
+    		displayImage(afterRotate);
+    		
+    		return afterRotate;
+    }
+    
+    protected void displayImage(int[][] matrix) { 	
+    		for(int[] row: matrix) {
+    			System.out.print("| ");
+    			for(int col : row) {
+    				System.out.printf("%d ",col);
+    			}
+    			System.out.println("|");
+    		}
+    		
+    }
+    
+    private int[][] rotateMatrix(int[][] matrix){
+    		for(int row = 0; row < (matrix.length-1)/2;row++) {
+    			for(int col = 0; col < matrix[row].length; col++) {    				    				
+    				int temp = matrix[row][col];
+        			matrix[row][col] = matrix[matrix.length-1-row][col];
+        			matrix[matrix.length-1-row][col] = temp;
+    			}
+    		}
+    		
+    	
+    		for(int row = 0; row < matrix.length;row++) {
+    			for(int col = row+1; col < matrix[row].length;col++) {    				if(row == col) continue;
+    				int temp = matrix[row][col];
+    				matrix[row][col] = matrix[col][row];
+    				matrix[col][row] = temp;
+    			}
+    		}
+    		return matrix;
+    }
 }
--- Rotate Image/src/RotatorTest.java
+++ Rotate Image/src/RotatorTest.java
@@ -1,4 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,7 +9,14 @@ class RotatorTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Rotator r = new Rotator();
+		int[][] sampleMatrix = {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] correctResult = {{7,4,1 },{8,5,2},{9,6,3}};
+		int[][] rotateImage = r.rotate(sampleMatrix);
+		
+		
+		
+		assertTrue(Arrays.deepEquals(correctResult, rotateImage));
 	}
 
 }
